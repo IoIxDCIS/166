8f224e26a1b987db984f6ac674cbaed4d0ff665b
IoIxD <alphaproject217@gmail.com>
2023-04-05 20:16:40 -0700

    ok

---
+++ what.sh
@@ -0,0 +1,5 @@
+xdg-open file:///home/gavin/School/166/html/template.html &
+xdg-open file:///home/gavin/School/166/html/about.html &
+xdg-open file:///home/gavin/School/166/html/contact.html &
+xdg-open file:///home/gavin/School/166/html/draggable-box.html &
+
