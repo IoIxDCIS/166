3f36bfa74f491f3a3aabde7c5160d2b5eb12d280
IoIxD <alphaproject217@gmail.com>
2023-04-13 13:20:42 -0700

    moved something to forEach

--- js/glitchfuck.js
+++ js/glitchfuck.js
@@ -28,17 +28,16 @@ $(window).on("load", () => {
     ];
 
     setInterval(() => {
+        c.width = window.innerWidth;
+        c.height = window.innerHeight;
         let ctx1 = new OffscreenCanvas(c.width, c.height).getContext("2d");
         ctx1.willReadFrequently = true;
         let ctx2 = new OffscreenCanvas(c.width, c.height).getContext("2d");
         ctx2.willReadFrequently = true;
+        
         colors.forEach((c, i) => {
             (!reverse[i] && c < 255) ? colors[i]+=5 : reverse[i]=true;
-            if (reverse[i] && c == 0) {
-                colors[i] = Math.random() * 255;
-            } 
             (reverse[i] && c > 0) ? colors[i]-=5 : reverse[i]=false;
-
         });
         
         let grd1;
@@ -51,12 +50,7 @@ $(window).on("load", () => {
         grd1.addColorStop(0, `rgb(${colors[0]},${colors[1]},${colors[2]})`);
         grd1.addColorStop(1, `rgb(${colors[3]},${colors[4]},${colors[5]})`);
     
-        let grd2;
-        if(type2 == 0 && type1 != 0) {
-            grd2 = ctx.createLinearGradient(0,0,c.width,c.height*dir[1]);
-        } else {
-            grd2 = ctx.createRadialGradient(c.width/2,c.height/2,0,c.width,c.height,c.width*c.height);
-        }
+        let grd2 = ctx.createLinearGradient(0,0,c.width,c.height*dir[1]);
         
         grd2.addColorStop(0, `rgb(${colors[6]},${colors[7]},${colors[8]})`);
         grd2.addColorStop(1, `rgb(${colors[9]},${colors[10]},${colors[11]})`);
@@ -70,12 +64,13 @@ $(window).on("load", () => {
         let d2 = ctx2.getImageData(0,0,c.width,c.height);
         let d3 = ctx1.createImageData(c.width,c.height);
         
-        for (let i = 0; i < d3.data.length; i += 4) {
-            d3.data[i] = d1.data[i] ^ d2.data[i]; // red
-            d3.data[i + 1] = d1.data[i+1] ^ d2.data[i+1]; // green
-            d3.data[i + 2] = d2.data[i+2] ^ d2.data[i+2]; // blue
-            d3.data[i + 3] = 255;
-        }
+        d3.data.forEach((_, i) => {
+            if(i+1 & 3 != 0) {
+                d3.data[i] = d1.data[i] ^ d2.data[i];
+            } else {
+                d3.data[i] = 255;
+            }
+        })
         ctx.putImageData(d3, 0, 0);
-    },100);
+    },1);
 })
