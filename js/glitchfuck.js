fc18f56c1b089f43ef97d32d0c648cf1134dcfb7
IoIxD <alphaproject217@gmail.com>
2023-04-13 12:40:29 -0700

    radial chance

--- js/glitchfuck.js
+++ js/glitchfuck.js
@@ -16,8 +16,10 @@ $(window).on("load", () => {
     ]
 
     let dir = [
-        Math.round(Math.random()),Math.round(Math.random()),Math.round(Math.random()),Math.round(Math.random()),
+        Math.round(Math.random()),Math.round(Math.random()),
     ]
+    let type1 = Math.round(Math.random());
+    let type2 = Math.round(Math.random());
     let reverse = [
         false,false,false,
         false,false,false,
@@ -39,11 +41,23 @@ $(window).on("load", () => {
 
         });
         
-        let grd1 = ctx.createLinearGradient(0,0,c.width*dir[0],c.height*dir[1]);
+        let grd1;
+        if(type1 == 0) {
+            grd1 = ctx.createLinearGradient(0,0,c.width*dir[0],c.height);
+        } else {
+            grd1 = ctx.createRadialGradient(c.width/2,c.height/2,0,c.width,c.height,c.width);
+        }
+        
         grd1.addColorStop(0, `rgb(${colors[0]},${colors[1]},${colors[2]})`);
         grd1.addColorStop(1, `rgb(${colors[3]},${colors[4]},${colors[5]})`);
     
-        let grd2 =  ctx.createLinearGradient(0,0,c.width*dir[2],c.height*dir[3]);
+        let grd2;
+        if(type2 == 0 && type1 != 0) {
+            grd2 = ctx.createLinearGradient(0,0,c.width,c.height*dir[1]);
+        } else {
+            grd2 = ctx.createRadialGradient(c.width/2,c.height/2,0,c.width,c.height,c.width*c.height);
+        }
+        
         grd2.addColorStop(0, `rgb(${colors[6]},${colors[7]},${colors[8]})`);
         grd2.addColorStop(1, `rgb(${colors[9]},${colors[10]},${colors[11]})`);
 
